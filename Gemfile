5c823252c07e186752b0a1dc74b18d5d5f6f12f1
Shimran George <shimran@nyu.edu>
2015-01-16 14:02:40 -0800

    Update phone.rb and add initial sinatra files

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source("https://rubygems.org")
+gem('sinatre')
+gem('sinatra-contrib')
--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,40 +1,46 @@
 class Contacts
-	
 	@@contacts = []
-
-	define_method(:initialize) do |name, phone_number|
-		@name	=	name
-		@phone_number = phone_number
-	end
-
-	define_method(:phone_number) do
-		@phone_number
-	end
-
-	define_method(:name) do
-		@name	
-	end
-
-
-	define_method(:save) do
-		@@contacts.push(self)
-	end
-
-	define_singleton_method(:all) do
-		@@contacts
-	end
-
-	define_singleton_method(:find_contact) do |search_for_contact|
-		found_countact	=	nil
-		@@contacts.each() do |contact|
-			if contact.name() == search_for_contact
-				found_countact = contact
-			end
+		define_method (:initialize) do |contact_name|
+			@contact_name = contact_name
+			@phone_list = []
+			#@id = @@artists.length().+1
 		end
-		found_countact
-	end
-	define_singleton_method(:clear) do 
-		@@contacts = []
-	end
+	
+		define_method(:contact_name) do
+		@contact_name
+		end
 
+		define_method(:phone_list) do
+		@phone_list
+		end
+
+		define_method(:add_contact) do |phones|
+		@phone_list.push(phones)
+		end
+
+		define_method(:id) do
+		@id	
+		end
+		
+		define_method(:save) do
+		@@contacts.push(self)
+		end
+
+		define_singleton_method(:all) do
+		@@contacts
+		end
+		
+		define_singleton_method(:clear) do
+		@@contacts = []
+		end
+
+		define_singleton_method(:find) do |search_contacts|
+		found_contact = nil
+			@@contacts.each() do |contact|
+				if contact.id() == search_contacts
+				found_contact = contact
+				end
+			end
+		found_contact
+		end
 end
